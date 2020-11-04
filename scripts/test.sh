04ee9ffe6e90bfe763c62f1fbaa6fab281017649
sammer1107 <qscesz871107@gmail.com>
2020-11-04 20:43:31 +0800

    Add feature: auto compare test output

--- scripts/test.sh
+++ scripts/test.sh
@@ -5,11 +5,28 @@ CALC_MOD=calc.ko
 
 EVAL=./eval
 
+NAN="NAN_INT"
+INF="INF_INT"
+
 test_op() {
-    local expression=$1 
-    echo "Testing " ${expression} "..."
+    local expression=$1
+    local ans=$2
+    echo -n "Testing " ${expression} "... "
     echo -ne ${expression}'\0' > $CALC_DEV
-    $EVAL $(cat $CALC_DEV)
+    local ret=$($EVAL $(cat $CALC_DEV))
+
+    if [[ "$ans" != $NAN && "$ans" != $INF ]]; then
+        ans=$(printf %lf\\n $ans)
+    fi
+
+    if [ "$ret" = "$ans" ]; then
+        echo -e "\e[32mPASSED\e[0m"
+        echo -e "==" $ans
+    else 
+        echo -e "\e[31mFAILED\e[0m"
+        echo -e "Got" $ret "instead of" $ans
+    fi
+    echo
 }
 
 if [ "$EUID" -eq 0 ]
@@ -26,48 +43,48 @@ sudo chmod 0666 $CALC_DEV
 echo
 
 # multiply
-test_op '6*7'
+test_op '6*7' 42
 
 # add
-test_op '1980+1'
+test_op '1980+1' 1981
 
 # sub
-test_op '2019-1'
+test_op '2019-1' 2018
 
 # div
-test_op '42/6'
-test_op '1/3'
-test_op '1/3*6+2/4'
-test_op '(1/3)+(2/3)'
-test_op '(2145%31)+23'
-test_op '0/0' # should be NAN_INT
+test_op '42/6' 7
+test_op '1/3' 0.333333
+test_op '1/3*6+2/4' 2.5
+test_op '(1/3)+(2/3)' 1
+test_op '(2145%31)+23' 29
+test_op '0/0' $NAN
 
 # binary
-test_op '(3%0)|0' # should be 0
-test_op '1+2<<3' # should be (1 + 2) << 3 = 24
-test_op '123&42' # should be 42
-test_op '123^42' # should be 81
+test_op '(3%0)|0' 0 
+test_op '1+2<<3' 24 # should be (1 + 2) << 3 = 24
+test_op '123&42' 42
+test_op '123^42' 81
 
 # parens
-test_op '(((3)))*(1+(2))' # should be 9
+test_op '(((3)))*(1+(2))' 9
 
 # assign
-test_op 'x=5, x=(x!=0)' # should be 1
-test_op 'x=5, x = x+1' # should be 6
+test_op 'x=5, x=(x!=0)' 1
+test_op 'x=5, x = x+1' 6
 
 # fancy variable name
-test_op 'six=6, seven=7, six*seven' # should be 42
-test_op '小熊=6, 維尼=7, 小熊*維尼' # should be 42
-test_op 'τ=1.618, 3*τ' # should be 3 * 1.618 = 4.854
-test_op '$(τ, 1.618), 3*τ()' # shold be 3 * 1.618 = 4.854
+test_op 'six=6, seven=7, six*seven' 42
+test_op '小熊=6, 維尼=7, 小熊*維尼' 42
+test_op 'τ=1.618, 3*τ' 4.854
+test_op '$(τ, 1.618), 3*τ()' 4.854
 
 # functions
-test_op '$(zero), zero()' # should be 0
-test_op '$(one, 1), one()+one(1)+one(1, 2, 4)' # should be 3
-test_op '$(number, 1), $(number, 2+3), number()' # should be 5
+test_op '$(zero), zero()' 0
+test_op '$(one, 1), one()+one(1)+one(1, 2, 4)' 3
+test_op '$(number, 1), $(number, 2+3), number()' 5
 
 # pre-defined function
-test_op 'nop()'
+test_op 'nop()' 0
 
 sudo rmmod calc
 
